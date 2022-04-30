746c347c462dc6d0617f336d2e781c74652646ed
Jim <b06608062@ntu.edu.tw>
2022-04-30 14:35:34 +0800

    1.0

---
+++ minesweeper/MainForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace minesweeper
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 73);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "00:00";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 500;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::minesweeper.Properties.Resources.win1;
+            this.pictureBox1.Location = new System.Drawing.Point(274, 152);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(256, 256);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.button1.Font = new System.Drawing.Font("Times New Roman", 72F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.DarkRed;
+            this.button1.Location = new System.Drawing.Point(630, 338);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(507, 156);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Play Again";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.PlayAgain);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1149, 795);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.Text = "Minesweeper";
+            this.Deactivate += new System.EventHandler(this.MainForm_Deactivate);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ minesweeper/MainForm.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Media;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace minesweeper
+{
+    public partial class MainForm : Form
+    {
+        Random myRandomNumberGenerator = new Random();
+        Bitmap[] labels = new Bitmap[9];
+        Bitmap boom, cover, flag, unkown, win1, win2, lose;
+        SoundPlayer start, win, booom;
+        PictureBox[,] board;
+        int row, col, boomNumber, time, coverNumber;
+        int width, windowWidth, windowHeight;
+
+        private void PlayAgain(object sender, EventArgs e)
+        {
+            start.Play();
+            timer2.Enabled = false;
+            pictureBox1.Visible = button1.Visible = false;
+            ClearBoard();
+            InitializeGame();
+        }
+
+        private void ClearBoard()
+        {
+            for (int r = 0; r < row; r++)
+            {
+                for (int c = 0; c < col; c++)
+                {
+                    Controls.Remove(board[r, c]);
+                }
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            pictureBox1.Image = pictureBox1.Image == win1 ? win2 : win1;
+        }
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+            this.TopMost = true;
+            labels[0] = Properties.Resources._0;
+            labels[1] = Properties.Resources._1;
+            labels[2] = Properties.Resources._2;
+            labels[3] = Properties.Resources._3;
+            labels[4] = Properties.Resources._4;
+            labels[5] = Properties.Resources._5;
+            labels[6] = Properties.Resources._6;
+            labels[7] = Properties.Resources._7;
+            labels[8] = Properties.Resources._8;
+            boom = Properties.Resources.boom1;
+            cover = Properties.Resources.cover;
+            flag = Properties.Resources.flag;
+            unkown = Properties.Resources.unkown;
+            win1 = Properties.Resources.win1;
+            win2 = Properties.Resources.win2;
+            lose = Properties.Resources.lose;
+            start = new SoundPlayer(Properties.Resources.start);
+            win = new SoundPlayer(Properties.Resources.win);
+            booom = new SoundPlayer(Properties.Resources.boom2);
+            row = 16;
+            col = 32;
+            board = new PictureBox[row, col];
+            boomNumber = row * col / 9;
+            windowWidth = System.Windows.Forms.SystemInformation.WorkingArea.Width;
+            windowHeight = System.Windows.Forms.SystemInformation.WorkingArea.Height;
+            width = windowWidth / (col + 2);
+            pictureBox1.Left = (windowWidth - pictureBox1.Width) / 2;
+            pictureBox1.Top = (windowHeight - pictureBox1.Height) / 3;
+            button1.Left = (windowWidth - button1.Width) / 2;
+            button1.Top = (windowHeight - button1.Height) / 3 * 2;
+            InitializeGame();
+        }
+
+        private void MainForm_Deactivate(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void InitializeGame()
+        {
+            label1.Text = "00:00";
+            time = 0;
+            coverNumber = row * col;
+            for (int r = 0; r < row; r++)
+            {
+                for (int c = 0; c < col; c++)
+                {
+                    board[r, c] = new PictureBox
+                    {
+                        Width = width,
+                        Height = width,
+                        Left = width + c * width,
+                        Top = 90 + r * width,
+                        Image = cover,
+                        SizeMode = PictureBoxSizeMode.StretchImage
+                    };
+                    board[r, c].MouseDown += Click;
+                    Controls.Add(board[r, c]);
+                }
+            }
+
+            for (int i = 0; i < boomNumber; i++)
+            {
+                SetBoom();
+            }
+            Mark();
+        }
+
+        private void SetBoom()
+        {
+            int r = myRandomNumberGenerator.Next(0, row);
+            int c = myRandomNumberGenerator.Next(0, col);
+            if (board[r, c].Tag != boom)
+            {
+                board[r, c].Tag = boom;
+            } else
+            {
+                SetBoom();
+            }
+        }
+
+        private void Mark()
+        {
+            for (int r = 0; r < row; r++)
+            {
+                for (int c = 0; c < col; c++)
+                {
+                    int count = 0;
+                    if (board[r, c].Tag != boom)
+                    {
+                        for (int i = r - 1; i <= r + 1; i++)
+                        {
+                            for (int j = c - 1; j <= c + 1; j++)
+                            {
+                                if (i >= 0 && i < row && j >=0 && j < col && board[i, j].Tag == boom)
+                                {
+                                    count++;
+                                }
+                            }
+                        }
+                        board[r, c].Tag = labels[count];
+                    }
+                }
+            }
+        }
+
+        private void Open(int r, int c)
+        {
+            if (board[r, c].Tag != boom && board[r, c].Image == cover)
+            {
+                coverNumber--;
+                board[r, c].Enabled = false;
+                if (board[r, c].Tag == labels[0])
+                {
+                    board[r, c].Image = labels[0];
+                    for (int i = r - 1; i <= r + 1; i++)
+                    {
+                        for (int j = c - 1; j <= c + 1; j++)
+                        {
+                            if (i >= 0 && i < row && j >= 0 && j < col)
+                            {
+                                Open(i, j);
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    board[r, c].Image = (Bitmap)board[r, c].Tag;
+                }
+            }
+        }
+
+        private new void Click(object sender, MouseEventArgs e)
+        {
+            if (!timer1.Enabled) timer1.Enabled = true;
+            PictureBox cell = (PictureBox)sender;
+
+            if (e.Button == MouseButtons.Left)
+            {
+                if (cell.Image != cover) return;
+
+                if (cell.Tag == boom)
+                {
+                    // game over
+                    booom.Play();
+                    timer1.Enabled = false;
+                    for (int r = 0; r < row; r++)
+                    {
+                        for (int c = 0; c < col; c++)
+                        {
+                            board[r, c].Image = (Bitmap)board[r, c].Tag;
+                        }
+                    }
+                    pictureBox1.Image = lose;
+                    pictureBox1.Visible = button1.Visible = true;
+                } else if (cell.Tag != labels[0])
+                {
+                    coverNumber--;
+                    cell.Enabled = false;
+                    cell.Image = (Bitmap)cell.Tag;
+                } else
+                {
+                    Open((cell.Top - 90) / width, (cell.Left - width) / width);
+                }
+            } else
+            {
+                cell.Image = cell.Image == cover ? flag : cell.Image == flag ? unkown : cover;
+            }
+
+            if (coverNumber == boomNumber)
+            {
+                // win
+                win.Play();
+                timer1.Enabled = false;
+                timer2.Enabled = true;
+                pictureBox1.Image = win1;
+                pictureBox1.Visible = button1.Visible = true;
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            time++;
+            label1.Text = $"{(time / 60).ToString().PadLeft(2, '0')}:{(time % 60).ToString().PadLeft(2, '0')}";
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+        }
+    }
+}
